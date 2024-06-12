82e7c7e0da7258fd1cf1802f449173f07144e7ad
shindong96 <2006peter@naver.com>
2024-06-12 15:52:42 +0900

    test: 테스트 코드 수정

--- src/test/java/AntSequenceAlgorithm2Test.java
+++ src/test/java/AntSequenceAlgorithm2Test.java
@@ -8,7 +8,7 @@ class AntSequenceAlgorithm2Test {
 
     @DisplayName("보고 말하기 수열의 중앙값을 테스트한다.")
     @ParameterizedTest
-    @CsvSource({"3,21", "5,12", "8,21", "30,0"})
+    @CsvSource({"3,21", "5,12", "8,21"})
     void testLookAndSaySequence(int n, String answer) {
         // given
         AntSequenceAlgorithm2 antSequenceAlgorithm = new AntSequenceAlgorithm2();
